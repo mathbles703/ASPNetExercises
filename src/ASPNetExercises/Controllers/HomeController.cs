d8063040bac25017625235737c5f53a22757b0d7
BMATHEW <mathbles703@gmail.com>
2016-05-18 10:04:55 -0400

    Lab 4 - Tag Helper and View Tray

--- src/ASPNetExercises/Controllers/HomeController.cs
+++ src/ASPNetExercises/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,6 +10,7 @@ namespace ASPNetExercises.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.Message = HttpContext.Session.GetString("Message");
             return View();
         }
     }
