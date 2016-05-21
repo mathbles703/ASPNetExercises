dbd0726d3517324331972bb2c0a005925a8eb3ea
BMATHEW <mathbles703@gmail.com>
2016-05-20 22:43:15 -0400

    Lab 7 - Added TrayModel so user can save their tray(order) of all items

--- src/ASPNetExercises/Controllers/TrayController.cs
+++ src/ASPNetExercises/Controllers/TrayController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Http;
+using ASPNetExercises.Models;
 using ASPNetExercises.Utils;
+using System;
+using System.Collections.Generic;
 namespace ASPNetExercises.Controllers
 {
     public class TrayController : Controller
     {
+        AppDbContext _db;
+        public TrayController(AppDbContext context)
+        {
+            _db = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -16,6 +24,41 @@ namespace ASPNetExercises.Controllers
             HttpContext.Session.SetString(SessionVars.Message, "Tray Cleared"); 
             // clear out current cart once order has been placed
         return Redirect("/Home");
+        }
+
+        // Add the tray, pass the session variable info to the db
+        public ActionResult AddTray()
+        {
+            // they can't add a Tray if they're not logged on
+            if (HttpContext.Session.GetString(SessionVars.User) == null)
+            {
+                return Redirect("/Login");
+            }
+            TrayModel model = new TrayModel(_db);
+            int retVal = -1;
+            string retMessage = "";
+            try
+            {
+                Dictionary<string, object> trayItems =
+                HttpContext.Session.GetObject<Dictionary<string, object>>(SessionVars.Tray);
+                retVal = model.AddTray(trayItems,
+                HttpContext.Session.GetString(SessionVars.User));
+                if (retVal > 0) // Tray Added
+                {
+                    retMessage = "Tray " + retVal + " Created!";
+                }
+                else // problem
+                {
+                    retMessage = "Tray not added, try again later";
+                }
+            }
+            catch (Exception ex) // big problem
+            {
+                retMessage = "Tray was not created, try again later! - " + ex.Message;
+            }
+            HttpContext.Session.Remove(SessionVars.Tray); // clear out current tray once persisted
+            HttpContext.Session.SetString(SessionVars.Message, retMessage);
+            return Redirect("/Home");
         }
     }
 }
