f5cbec771a702fff9453d483bf1b69b20924edd0
BMATHEW <mathbles703@gmail.com>
2016-06-02 16:07:08 -0400

    Adjusted formatting on jsx table example

--- src/ASPNetExercises/wwwroot/js/bootstrapreactexample.jsx
+++ src/ASPNetExercises/wwwroot/js/bootstrapreactexample.jsx
@@ -19,13 +19,13 @@ var ListItem = React.createClass({
     },
     render: function () {
         return (
-        <ListGroupItem>
-        <span style={idStyle}>{this.props.item.id}</span>
-        <span style={idStyle}>{this.props.item.data}</span>
-        <span style={idStyle}>{this.props.item.data2}</span>
-    </ListGroupItem>
-)
-}
+            <ListGroupItem>
+                <span style={idStyle}>{this.props.item.id}</span>
+                <span style={idStyle}>{this.props.item.data}</span>
+                <span style={idStyle}>{this.props.item.data2}</span>
+            </ListGroupItem>
+        )
+    }
 });
 //
 // ListItems Component
@@ -34,35 +34,35 @@ var ListItems = React.createClass({
     getInitialState: function () {
         return ({
             listitems: [
-            { "id": 1, "data": "data for 1", "data2": "more data for 1"},
-            { "id": 2, "data": "data for 2", "data2": "more data for 2"},
-            { "id": 3, "data": "data for 3", "data2": "more data for 3"}
+                { "id": 1, "data": "data for 1", "data2": "more data for 1"},
+                { "id": 2, "data": "data for 2", "data2": "more data for 2"},
+                { "id": 3, "data": "data for 3", "data2": "more data for 3"}
             ]});
     },
     render: function () {
         var items = this.state.listitems.map(item =>
             <ListItem item={item} key={item.id} />
-);
-return (
-<div style={{fontSize:"xx-large"}}>
-<div className="col-xs-2">&nbsp;</div>
-<div className="panel-body col-xs-8">
-<div>
-<div className="text-center navbar navbar-default" style={{top:"25px", position:"relative"}}>
-<div className="col-xs-2 top10">Key</div>
-<div className="col-xs-5 top10">Data</div>
-<div className="col-xs-5 top10">xData</div>
-</div>
-<ListGroup>
-{items}
-</ListGroup>
-</div>
-</div>
-</div>
-)
-}
+        );
+        return (
+            <div style={{fontSize:"xx-large"}}>
+            <div className="col-xs-2">&nbsp;</div>
+            <div className="panel-body col-xs-8">
+                <div>
+                    <div className="text-center navbar navbar-default" style={{top:"25px", position:"relative"}}>
+                        <div className="col-xs-2 top10">Key</div>
+                        <div className="col-xs-5 top10">Data</div>
+                        <div className="col-xs-5 top10">xData</div>
+                    </div>
+                    <ListGroup>
+                    {items}
+                    </ListGroup>
+                </div>
+            </div>
+            </div>
+        )
+    }
 })
 ReactDOM.render(
-<ListItems />,
-document.getElementById("example") // html tag
+    <ListItems />,
+    document.getElementById("example") // html tag
 )
